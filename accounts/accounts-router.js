93a698cfc7c6aa3137a85d8c5846a63064682022
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-07 22:19:39 -0400

    Get account by ID

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -18,4 +18,20 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const {id} = req.params;
+
+  try {
+    const [account] = await db('accounts').where({id});
+
+    if (account) {
+      res.status(200).json(account);
+    } else {
+      res.status(404).json({message: `account with id ${id} not found`});
+    }
+  } catch (err) {
+    res.status(500).json({message: 'error retrieving the account'});
+  }
+});
+
 module.exports = router;
